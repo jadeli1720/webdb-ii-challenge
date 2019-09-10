0d1fd7c32e118a01e80cb1ebd6db64c237589e78
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-10 15:25:46 -0600

    Create get endpoint and successfully retrieved the data

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -3,8 +3,17 @@ const db = require('../data/db-config'); //<<<<<<< Will be used to require data 
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello. Do you see me?')
+
+router.get('/', (req,res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(err => {
+            console.log('GET', err)
+            res.status(500).json({ error: 'Failed to retrieve cars data' })
+        });
+
 })
 
 module.exports = router;
