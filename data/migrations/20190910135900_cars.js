9c801184a280960c4f5ab04848b63ac33dc58306
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-10 14:33:04 -0600

    coded  migrations schema and ran pending migrations

--- data/migrations/20190910135900_cars.js
+++ data/migrations/20190910135900_cars.js
@@ -3,27 +3,41 @@ exports.up = function (knex) {
     //Don't forget return!!
     return knex.schema.createTable('cars', tbl => {
         //id, primary key, auto-increment, integer
-        tbl.increments()
+        tbl.increments('carsId');
 
-        //vin: number, unique, required(not null), 17 characters in length?? has to be 17 bigInteger?
+        //vin: numbers and letters, unique, required(not null), 17 characters in length
         tbl
             .string('vin', 17)
             .unique()
             .notNullable();
         
         //make: string, required
+        tbl
+            .string('make')
+            .notNullable();
 
         //model: string, required
+        tbl
+            .string('model')
+            .notNullable();
 
-        //milage: integer, required
+        //milage: integer, required, up to 6
+        tbl
+            .string('milage', 6)
+            .notNullable();
 
         //transmission type: string, required
+        tbl
+            .string('transType')
+            .notNullable();
 
         //Title status: string, not required
+        tbl.string('titleStatus')
 
     })
 };
 
 exports.down = function (knex) {
     //Don't forget return!!
+    return knex.schema.dropTableIfExists('cars')
 };
